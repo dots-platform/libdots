8e87bda3949bf9c07bc2b75a115daed30356ca85
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-04-25 08:24:50 -0700

    Refactor DotsError type to take return value for Unknown error

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,22 +6,22 @@ pub mod msg;
 #[derive(Debug)]
 pub enum DotsError {
     // Generic error used for invalid return value.
-    Unknown = 0,
+    Unknown(i32),
 
-    Libc = -1,
-    Interface = -2,
-    Invalid = -3,
-    Internal = -4,
+    Libc,
+    Interface,
+    Invalid,
+    Internal,
 }
 
 impl DotsError {
     fn from_ret(ret: i32) -> DotsError {
         match ret {
-            x if x == DotsError::Libc as i32 => DotsError::Libc,
-            x if x == DotsError::Interface as i32 => DotsError::Interface,
-            x if x == DotsError::Invalid as i32 => DotsError::Invalid,
-            x if x == DotsError::Internal as i32 => DotsError::Internal,
-            _ => DotsError::Unknown,
+            -1 => DotsError::Libc,
+            -2 => DotsError::Interface,
+            -3 => DotsError::Invalid,
+            -4 => DotsError::Internal,
+            _ => DotsError::Unknown(ret),
         }
     }
 }
