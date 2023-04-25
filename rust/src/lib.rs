2d5f0dc2789ef55d98350b8e4bb671b997364cc2
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-04-25 09:42:39 -0700

    Implement fmt::Display and Error traits on DotsError

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,9 @@ mod ffi;
 pub mod env;
 pub mod msg;
 
+use std::error::Error;
+use std::fmt;
+
 #[derive(Debug)]
 pub enum DotsError {
     // Generic error used for invalid return value.
@@ -25,5 +28,20 @@ impl DotsError {
         }
     }
 }
+
+impl fmt::Display for DotsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            DotsError::Libc => String::from("libdots libc error"),
+            DotsError::Interface => String::from("libdots interface error"),
+            DotsError::Invalid => String::from("libdots invalid argument error"),
+            DotsError::Internal => String::from("libdots internal error"),
+            DotsError::Unknown(ret) => format!("libdots unknown error: {}", ret),
+        };
+        f.write_str(s.as_str())
+    }
+}
+
+impl Error for DotsError {}
 
 pub type DotsResult<T> = Result<T, DotsError>;
