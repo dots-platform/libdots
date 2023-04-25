d2119930c268898ea8a6ba9a673cb162ffe1fef5
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-04-25 08:44:07 -0700

    Use slices instead of vectors for msg::{send,recv}

--- rust/src/msg.rs
+++ rust/src/msg.rs
@@ -4,7 +4,7 @@ use std::mem::MaybeUninit;
 use crate::*;
 use crate::ffi as ffi;
 
-pub fn send(buf: &Vec<u8>, recipient: usize, tag: i32) -> DotsResult<()> {
+pub fn send(buf: &[u8], recipient: usize, tag: i32) -> DotsResult<()> {
     unsafe {
         let ret = ffi::dots_msg_send(buf.as_ptr() as *const c_void, buf.len(), recipient, tag);
         if ret != 0 {
@@ -15,7 +15,7 @@ pub fn send(buf: &Vec<u8>, recipient: usize, tag: i32) -> DotsResult<()> {
     Ok(())
 }
 
-pub fn recv(buf: &mut Vec<u8>, recipient: usize, tag: i32) -> DotsResult<usize> {
+pub fn recv(buf: &mut [u8], recipient: usize, tag: i32) -> DotsResult<usize> {
     unsafe {
         let mut bytes_received: usize = MaybeUninit::uninit().assume_init();
         let ret = ffi::dots_msg_recv(buf.as_ptr() as *mut c_void, buf.len(), recipient, tag, &mut bytes_received);
