698252117f79be321e5ba2a85231f60acc1884f0
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-05-12 10:41:38 -0700

    Rust: Don't copy libdots source directory if it's a symlink

--- rust/build.rs
+++ rust/build.rs
@@ -1,6 +1,7 @@
 extern crate bindgen;
 
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
@@ -8,13 +9,21 @@ use bindgen::CargoCallbacks;
 use fs_extra::dir;
 
 fn main() {
-    let lib_path = PathBuf::from(".").canonicalize().unwrap().join("libdots");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let out_lib_path = out_path.join("libdots");
+    let lib_path = &PathBuf::from(".").canonicalize().unwrap().join("libdots");
+    let out_path = &PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut out_lib_path = &out_path.join("libdots");
 
-    let mut copy_options = dir::CopyOptions::new();
-    copy_options.skip_exist = true;
-    dir::copy(&lib_path, &out_path, &copy_options).unwrap();
+    if fs::read_link(&lib_path).is_ok() {
+        // The library path is a symlink, so just use the link as the build
+        // directory.
+        out_lib_path = lib_path;
+    } else {
+        // The library path is a source directory, so copy it to OUT_DIR so we
+        // don't modify it.
+        let mut copy_options = dir::CopyOptions::new();
+        copy_options.skip_exist = true;
+        dir::copy(&lib_path, &out_path, &copy_options).unwrap();
+    }
 
     println!("cargo:rustc-link-search={}", out_lib_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=dots");
